7f4570360459dbdf05fddecf4f1a6c6de39d071d
inmacc96 <60236648+Inmacc96@users.noreply.github.com>
2025-01-23 16:53:04 +0100

    layout the no-search-results and empty notes view

--- app/ui/NotesList.tsx
+++ app/ui/NotesList.tsx
@@ -2,6 +2,7 @@ import { useMemo } from "react";
 import { useStore } from "../lib/store";
 import NoteCard from "./NoteCard";
 import { sortNotes } from "../lib/utils";
+import Image from "next/image";
 
 const NotesList = () => {
   const notes = useStore((state) => state.notes);
@@ -12,12 +13,24 @@ const NotesList = () => {
     return sortedNotes.filter((note) => note.title.includes(search));
   }, [search, sortedNotes]);
 
-  if (data.length === 0 && search)
+  if (data.length === 0) {
+    const imageSrc = search ? "/search-results.svg" : "/empty-notes.svg";
+    const imageAlt = search ? "no-search-results" : "empty-notes";
+    const message = search ? "No notes found" : "You don't have any notes";
+
     return (
-      <section>
-        <p>No notes found</p>
+      <section className="pt-12 w-full flex flex-col items-center justify-center gap-6">
+        <Image
+          src={imageSrc}
+          width={160}
+          height={160}
+          alt={imageAlt}
+          priority={true}
+        />
+        <p className="text-gray-900 font-medium text-lg">{message}</p>
       </section>
     );
+  }
 
   return (
     <section className="grid grid-cols-[repeat(auto-fill,_minmax(300px,_1fr))] gap-6">
