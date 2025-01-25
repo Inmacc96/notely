e314553a9f2713dbf22ffbc7d50e3992c25dcecd
inmacc96 <60236648+Inmacc96@users.noreply.github.com>
2025-01-25 12:23:13 +0100

    persist notes in localstorage

--- app/lib/store.ts
+++ app/lib/store.ts
@@ -1,8 +1,10 @@
 import { create } from "zustand";
 import { ActionType, Category, Note } from "./type";
+import { persist } from "zustand/middleware";
 
 interface Store {
   notes: Note[];
+  setNotes: (notes: Note[]) => void;
   addNote: (note: Note) => void;
   editNote: (note: Note) => void;
   deleteNote: (id: string) => void;
@@ -18,47 +20,58 @@ interface Store {
   toggleShowCompletedNotes: () => void;
 }
 
-export const useStore = create<Store>((set, get) => ({
-  notes: [],
-  completedNotes: [],
-  addNote: (note: Note) => {
-    set({ notes: [...get().notes, note] });
-  },
-  editNote: (note: Note) => {
-    const updatedNotes = get().notes.map((item) =>
-      item.id === note.id ? note : item
-    );
-    set({ notes: updatedNotes });
-  },
-  deleteNote: (id: string) => {
-    set({ notes: get().notes.filter((item) => item.id !== id) });
-  },
-  toggleCompleted: (id: string) => {
-    set({
-      notes: get().notes.map((item) =>
-        item.id === id
-          ? { ...item, completedAt: item.completedAt ? undefined : new Date() }
-          : item
-      ),
-    });
-  },
-  modal: { isShow: false, actionType: "create" },
-  showModal: (actionType: ActionType, payload?: unknown) => {
-    set(() => ({ modal: { isShow: true, actionType, payload } }));
-  },
-  closeModal: () => {
-    set(() => ({ modal: { ...get().modal, isShow: false } }));
-  },
-  search: "",
-  setSearch: (value: string) => {
-    set({ search: value });
-  },
-  filter: "All",
-  setFilter: (value: Category | "All") => {
-    set({ filter: value });
-  },
-  showCompletedNotes: false,
-  toggleShowCompletedNotes: () => {
-    set({ showCompletedNotes: !get().showCompletedNotes });
-  },
-}));
+export const useStore = create<Store>()(
+  persist(
+    (set, get) => ({
+      notes: [],
+      completedNotes: [],
+      setNotes: (notes: Note[]) => {
+        set({ notes });
+      },
+      addNote: (note: Note) => {
+        set({ notes: [...get().notes, note] });
+      },
+      editNote: (note: Note) => {
+        const updatedNotes = get().notes.map((item) =>
+          item.id === note.id ? note : item
+        );
+        set({ notes: updatedNotes });
+      },
+      deleteNote: (id: string) => {
+        set({ notes: get().notes.filter((item) => item.id !== id) });
+      },
+      toggleCompleted: (id: string) => {
+        set({
+          notes: get().notes.map((item) =>
+            item.id === id
+              ? {
+                  ...item,
+                  completedAt: item.completedAt ? undefined : new Date(),
+                }
+              : item
+          ),
+        });
+      },
+      modal: { isShow: false, actionType: "create" },
+      showModal: (actionType: ActionType, payload?: unknown) => {
+        set(() => ({ modal: { isShow: true, actionType, payload } }));
+      },
+      closeModal: () => {
+        set(() => ({ modal: { ...get().modal, isShow: false } }));
+      },
+      search: "",
+      setSearch: (value: string) => {
+        set({ search: value });
+      },
+      filter: "All",
+      setFilter: (value: Category | "All") => {
+        set({ filter: value });
+      },
+      showCompletedNotes: false,
+      toggleShowCompletedNotes: () => {
+        set({ showCompletedNotes: !get().showCompletedNotes });
+      },
+    }),
+    { name: "notes", partialize: (state) => state.notes }
+  )
+);
--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,28 @@
 "use client";
+import { useEffect } from "react";
 import { useStore } from "./lib/store";
 import Header from "./ui/Header";
 import Modal from "./ui/Modal";
 import NotesView from "./ui/NotesView";
+import { Note } from "./lib/type";
 
 export default function Home() {
   const isShowModal = useStore((state) => state.modal.isShow);
+  const setNotes = useStore((state) => state.setNotes);
+
+  useEffect(() => {
+    const notes: Note[] =
+      JSON.parse(localStorage.getItem("notes") ?? "[]")?.state ?? [];
+
+    setNotes(
+      notes.map((note) => ({
+        ...note,
+        completedAt: note.completedAt ? new Date(note.completedAt) : undefined,
+        updatedAt: new Date(note.updatedAt),
+      }))
+    );
+  }, [setNotes]);
+
   return (
     <>
       <Header />
