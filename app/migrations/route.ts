836f1529ea4c53afe116c0158ab83a07150e9002
inmacc96 <60236648+Inmacc96@users.noreply.github.com>
2025-01-25 13:42:42 +0100

    setting prostgres database and create note table

---
+++ app/migrations/route.ts
@@ -0,0 +1,23 @@
+import { sql } from "@vercel/postgres";
+
+const createNoteTable = async () => {
+  await sql`
+    CREATE TABLE IF NOT EXISTS note (
+        id UUID DEFAULT gen_random_uuid() PRIMARY KEY,
+        title TEXT NOT NULL,
+        category TEXT NOT NULL,
+        description TEXT,
+        completed_at TIMESTAMP,
+        updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+    );
+`;
+};
+
+export const GET = async () => {
+  try {
+    await createNoteTable();
+    return Response.json({ message: "Note table created successfully" });
+  } catch (error) {
+    return Response.json({ error }, { status: 500 });
+  }
+};
